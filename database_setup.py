299a5a8bda119564e27d3bfbeec49cf8c7ab35f9
krishna-pandey-git <naveen.pnd@gmail.com>
2015-10-10 02:49:21 +0530

    first commit

---
+++ database_setup.py
@@ -0,0 +1,73 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+################### Declaring Classes ###########
+class User(Base):
+        __tablename__='user'
+        id = Column(
+                Integer, primary_key = True)
+        name = Column(
+                String(80), nullable = False)
+        email = Column(String(80),
+                       nullable = False)
+        picture = Column(
+                String, nullable = True)
+
+class Restaurant(Base):
+	__tablename__ ='restaurant'
+	id = Column(
+		Integer, primary_key = True)
+	name = Column(
+		String(80),nullable = False)
+	user_id = Column(
+                Integer, ForeignKey('user.id'))
+	user = relationship(User)
+	
+	@property
+        def serialize(self):
+        #return object data in easily serializable format
+                return {
+                'name': self.name,
+                'id': self.id
+                }
+
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+	name = Column(
+		String(80),nullable=False)
+	id = Column(Integer, primary_key=True)
+	course = Column(String(250))
+	description = Column(String(250))
+	price = Column(String(8))
+	restaurant_id = Column(
+		Integer, ForeignKey('restaurant.id'))
+	user_id = Column(
+                Integer, ForeignKey('user.id'))
+	user = relationship(User)
+	restaurant = relationship(Restaurant)
+        @property
+        def serialize(self):
+        #return object data in easily serializable format
+                return {
+                'name': self.name,
+                'description': self.description,
+                'id': self.id,
+                'price': self.price,
+                'course': self.course
+                }
+
+############insert at the end ################
+
+engine = create_engine(
+'sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
